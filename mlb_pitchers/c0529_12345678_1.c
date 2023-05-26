d07068ccc43e62556fbed2eb265329b7d4f88f3b
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-27 01:08:50 +0900

    하나 더 선언.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -25,6 +25,9 @@ struct pitcher rhj = {
     "Ryu", "H", "LAD", 6, 1, 1.52, 9, 9, 0, 0, 59.1, 40, 10, 10, 6, 4, 59, .190, 0.74
 };
 
+struct pitcher davies = {
+    "Davies", "Z", "MIL", 5, 0, 1.54, 9, 9, 0, 0, 52.2, 46, 14, 9, 4, 16, 36, .240, 1.18
+};
 
 int main(int argc, char const *argv[])
 {
