1188476098528f2cc71ccecb0ab39c767b46016d
Kiyong Kim <empty email>
2023-05-26 20:33:36 +0900

    mlb_pitchers.txt 에 있는 한 줄을 복사해 오고 각 항목에 대해 조사한 결과를 주석으로 쓴다.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+// from MLB.com:
+// RK  Player  Team    W   L   ERA▲    G   GS  SV  SVO IP  H   R   ER  HR  BB  SO  AVG WHIP
+// 1    Ryu, H LAD 6   1   1.52    9   9   0   0   59.1    40  10  10  6   4   59  .190    0.74
+
+// Tm = Team, W = Wins, L = Loses, ERA = Earned Runs Average
+// G = Games, GS = Games Started, SV = Saves, SVO = Save Opportunity
+// IP = Innings Pitched, H = Hits, R = Runs, ER = Earned Runs
+// HR = Homeruns, BB = Base on balls (walk), SO = Strikeouts
+// AVG = Hit Average, WHIP = (Walks+Hits)/IP
+
 int int main(int argc, char const *argv[])
 {
     return 0;
