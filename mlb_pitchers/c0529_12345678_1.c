71e7877569fb6596b0521f9850fa59305a204ef0
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-27 01:16:03 +0900

    별도 파일로 분리.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
-
-// from MLB.com:
-// RK  Player  Team    W   L   ERA▲    G   GS  SV  SVO IP  H   R   ER  HR  BB  SO  AVG WHIP
-// 1    Ryu, H LAD 6   1   1.52    9   9   0   0   59.1    40  10  10  6   4   59  .190    0.74
-
-// Tm = Team, W = Wins, L = Loses, ERA = Earned Runs Average
-// G = Games, GS = Games Started, SV = Saves, SVO = Save Opportunity
-// IP = Innings Pitched, H = Hits, R = Runs, ER = Earned Runs
-// HR = Homeruns, BB = Base on balls (walk), SO = Strikeouts
-// AVG = Hit Average, WHIP = (Walks+Hits)/IP
-
-typedef struct pitcher {
-    char lastName[20], firstName[4];
-    char team[8];
-    int win, lose;
-    float era;
-    int games, gamesStarted, saves, saveOppertunities;
-    float inningsPitched;
-    int hits, runs, earnedRuns, homeRuns, basesOnBalls, strikeOuts;
-    float battingAverage, whip;
-} Pitcher;
-
-Pitcher pitchers[] = {
-    { "Ryu", "H", "LAD", 6, 1, 1.52, 9, 9, 0, 0, 59.1, 40, 10, 10, 6, 4, 59, .190, 0.74 },
-    { "Davies", "Z", "MIL", 5, 0, 1.54, 9, 9, 0, 0, 52.2, 46, 14, 9, 4, 16, 36, .240, 1.18 }
-};
-
-const int pitcher_count = sizeof(pitchers) / sizeof(pitchers[0]);
+#include "pitcher.h"
 
 int main(int argc, char const *argv[])
 {
---
+++ mlb_pitchers/pitcher.h
@@ -0,0 +1,22 @@
+// from MLB.com:
+// RK  Player  Team    W   L   ERA▲    G   GS  SV  SVO IP  H   R   ER  HR  BB  SO  AVG WHIP
+// 1    Ryu, H LAD 6   1   1.52    9   9   0   0   59.1    40  10  10  6   4   59  .190    0.74
+
+// Tm = Team, W = Wins, L = Loses, ERA = Earned Runs Average
+// G = Games, GS = Games Started, SV = Saves, SVO = Save Opportunity
+// IP = Innings Pitched, H = Hits, R = Runs, ER = Earned Runs
+// HR = Homeruns, BB = Base on balls (walk), SO = Strikeouts
+// AVG = Hit Average, WHIP = (Walks+Hits)/IP
+
+typedef struct pitcher {
+    char lastName[20], firstName[4];
+    char team[8];
+    int win, lose;
+    float era;
+    int games, gamesStarted, saves, saveOppertunities;
+    float inningsPitched;
+    int hits, runs, earnedRuns, homeRuns, basesOnBalls, strikeOuts;
+    float battingAverage, whip;
+} Pitcher;
+
+extern const int pitcher_count;
