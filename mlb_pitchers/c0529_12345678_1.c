c89a8256bd2e8ee256541cf2bb5a0e7e1caa0ef2
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-28 18:02:41 +0900

    승 수를 비교하는 함수 추가하고 승수로 정렬한다. 승수는 클수록 앞에 나와야 하므로 p2 에서 p1 을 뺀다.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pitcher.h"
 
-int values[] = {
-    
-};
+int Pitcher_cmp_win(const void *a, const void *b)
+{
+    const Pitcher *p1 = (const Pitcher *)a;
+    const Pitcher *p2 = (const Pitcher *)b;
+
+    int win_diff = p2->win - p1->win;
+    return win_diff;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Pitcher count = %d\n", pitcher_count);
+    Pitcher_printAll(10);
+    qsort(pitchers,
+        pitcher_count,
+        sizeof(pitchers[0]),
+        Pitcher_cmp_win);
+    printf("----------\n");
     Pitcher_printAll(10);
     return 0;
 }
