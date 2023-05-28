8475c58ffa45c7ec86265433d70a5de6135cfba5
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-28 17:27:31 +0900

    오름차순 정렬.

--- mlb_pitchers/sort.c
+++ mlb_pitchers/sort.c
@@ -6,6 +6,13 @@ int values[] = {
     945, 221, 543, 113, 909, 104, 294, 451, 569, 690,
 };
 
+int num_cmp(const void *a, const void *b)
+{
+  int v1 = *(int *)a;
+  int v2 = *(int *)b;
+  return v1 - v2;
+}
+
 void printValues(void)
 {
     for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++) {
@@ -17,5 +24,8 @@ void printValues(void)
 int main(int argc, char const *argv[])
 {
     printValues();
+    qsort(values, sizeof(values) / sizeof(values[0]), sizeof(values[0]), num_cmp);
+    printValues();
+
     return 0;
 }
