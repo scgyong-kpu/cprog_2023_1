2c8bf57582dc6194e0c611d7101f4c38757c4bea
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-28 18:06:00 +0900

    승/패 를 모두 비교하는 버전으로 만들어 본다.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include "pitcher.h"
 
-int Pitcher_cmp_win(const void *a, const void *b)
+int Pitcher_cmp_win_lose(const void *a, const void *b)
 {
     const Pitcher *p1 = (const Pitcher *)a;
     const Pitcher *p2 = (const Pitcher *)b;
 
+    // 먼저 승수를 비교한다
     int win_diff = p2->win - p1->win;
-    return win_diff;
+    if (win_diff != 0) {
+        return win_diff;
+    }
+    // 승수가 같으편 패 수를 비교한다
+    int lose_diff = p1->lose - p2->lose;
+    return lose_diff;
 }
 
 int main(int argc, char const *argv[])
@@ -17,7 +23,7 @@ int main(int argc, char const *argv[])
     qsort(pitchers,
         pitcher_count,
         sizeof(pitchers[0]),
-        Pitcher_cmp_win);
+        Pitcher_cmp_win_lose);
     printf("----------\n");
     Pitcher_printAll(10);
     return 0;
