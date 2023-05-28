644a03d0d5a490e0f5a4719de3cfa0a7a14f3e0a
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-28 18:07:37 +0900

    방어율을 비교하는 함수를 만들고 방어율로 정렬한다.

--- mlb_pitchers/c0529_12345678_1.c
+++ mlb_pitchers/c0529_12345678_1.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include "pitcher.h"
 
+int Pitcher_cmp_era(const void *a, const void *b)
+{
+    const Pitcher *p1 = (const Pitcher *)a;
+    const Pitcher *p2 = (const Pitcher *)b;
+
+    if (p1->era < p2->era) {
+        return -1;
+    } else if (p1->era > p2->era) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int Pitcher_cmp_win_lose(const void *a, const void *b)
 {
     const Pitcher *p1 = (const Pitcher *)a;
@@ -23,7 +37,7 @@ int main(int argc, char const *argv[])
     qsort(pitchers,
         pitcher_count,
         sizeof(pitchers[0]),
-        Pitcher_cmp_win_lose);
+        Pitcher_cmp_era);
     printf("----------\n");
     Pitcher_printAll(10);
     return 0;
